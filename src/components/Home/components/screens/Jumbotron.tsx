081e69f1908cf3d78a7daf14753219566c39998b
Nicolas Bazille <oltodo@msn.com>
2024-03-11 14:05:17 +0100

    Improve jumbotron layout

--- src/components/Home/components/screens/Jumbotron.tsx
+++ src/components/Home/components/screens/Jumbotron.tsx
@@ -1,12 +1,18 @@
 import { useMotionValueEvent, useScroll, motion } from "framer-motion";
-import { forwardRef, useState } from "react";
+import { forwardRef, useEffect, useState } from "react";
 import clsx from "clsx/lite";
+
+import ArrowImage from "../../images/arrow.svg";
 
 export const Jumbotron = forwardRef<HTMLDivElement>(function Jumbotron(_, ref) {
   const [scrollIndicatorDisplayed, setScrollIndicatorDisplayed] =
-    useState(true);
+    useState(false);
 
   const { scrollY } = useScroll();
+
+  useEffect(() => {
+    setScrollIndicatorDisplayed(true);
+  }, []);
 
   useMotionValueEvent(scrollY, "change", (latest) => {
     setScrollIndicatorDisplayed(latest === 0);
@@ -18,12 +24,12 @@ export const Jumbotron = forwardRef<HTMLDivElement>(function Jumbotron(_, ref) {
         backgroundImage: `url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA2MTAuNjYgNTIiIHByZXNlcnZlQXNwZWN0UmF0aW89InhNaWRZTWluIj4KICA8cGF0aCBmaWxsPSJub25lIiBzdHJva2U9IiNGRkQxOTciIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIgogICAgZD0iTS4zMyAyNmM5NS4yOTEgMzMuMzMzIDE5Ni45NTggMzMuMzMzIDMwNSAwIDEwOC4wNDMtMzMuMzMzIDIwOS43MS0zMy4zMzMgMzA1IDAiIC8+Cjwvc3ZnPg==')`,
       }}
       className={clsx(
-        "relative flex h-[100vh] w-[100vw] items-end gap-[10vmin] bg-[length:30vw] bg-repeat p-[5vmin] py-[10vmin]",
-        "portrait:flex-col portrait:items-start portrait:justify-end",
+        "relative flex h-[100vh] w-[100vw] gap-[5vh] bg-[length:40vh] bg-repeat p-[5vmin] pt-20 md:pt-32",
+        "flex-col items-start justify-end",
       )}
       ref={ref}
     >
-      <div className="flex flex-col gap-4 font-title text-[12vh] leading-none tracking-tight text-terracotta portrait:text-[10vw]">
+      <div className="my-auto flex flex-col gap-4 font-title text-[min(9.5vw,12vh,150px)] leading-none tracking-tight text-terracotta">
         <div>
           <span>ACCUEILLIR.</span>
         </div>
@@ -37,21 +43,23 @@ export const Jumbotron = forwardRef<HTMLDivElement>(function Jumbotron(_, ref) {
           <span>ACCOMPAGNER.</span>
         </div>
       </div>
-
-      <motion.div
+      <div
         className={clsx(
-          "text-[12vmax] leading-none duration-[3s]",
-          scrollIndicatorDisplayed && "animate-bounce",
+          "transition-opacity duration-0",
+          scrollIndicatorDisplayed
+            ? "opacity-100 delay-[3s]"
+            : "opacity-0 delay-0",
         )}
-        transition={{
-          duration: scrollIndicatorDisplayed ? 1 : 0.1,
-          delay: scrollIndicatorDisplayed ? 3 : 0,
-        }}
-        animate={{ opacity: scrollIndicatorDisplayed ? 1 : 0 }}
-        initial={{ opacity: 0 }}
       >
-        ↓
-      </motion.div>
+        <ArrowImage
+          className={clsx(
+            "h-[12vh] duration-[3s] md:h-[15vh]",
+            scrollIndicatorDisplayed && "animate-bounce",
+          )}
+        >
+          ↓
+        </ArrowImage>
+      </div>
     </div>
   );
 });
